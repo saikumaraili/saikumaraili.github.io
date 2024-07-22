344da69a3f6b23ef78f9ca0df6290c475c0e1caa
sai kumar aili <sk.saikumaraili@gmail.com>
2024-07-22 04:27:48 -0400

    gh-pages build failedRevert "updated basic site requirements"
    This reverts commit 37f319d052134add3658748e2480c0325538e9df.

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Download Resume
+            Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
