2eb9fc195849691dae49235cf9974d978d385df3
sai kumar aili <sk.saikumaraili@gmail.com>
2024-07-22 04:46:51 -0400

    updated deployment branch to main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   trailingSlash: false,
-
+  deploymentBranch: 'main',
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
